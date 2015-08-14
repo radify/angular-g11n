255bba98089d961e1ef381f590fb2479745226cf
John F. Mercer <john@radify.io>
2015-08-14 11:47:05 -0400

    Fixes spacing in karma.conf.js
    * Replaces previous indentation mess with two spaces per indent.

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,30 @@
 module.exports = function(config) {
-	config.set({
+  config.set({
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+  // test results reporter to use
+  // possible values: 'dots', 'progress'
+  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+  reporters: ['progress'],
 
-    files: [
-        'node_modules/angular/angular.js',
-        'node_modules/angular-mocks/angular-mocks.js',
-        'src/*.js',
-        'spec/*.js'
-    ],
+  files: [
+    'node_modules/angular/angular.js',
+    'node_modules/angular-mocks/angular-mocks.js',
+    'src/*.js',
+    'spec/*.js'
+  ],
 
-    // web server port
-    port: 9876,
+  // web server port
+  port: 9876,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+  // enable / disable colors in the output (reporters and logs)
+  colors: true,
 
-		exclude: [],
-		frameworks: ['jasmine'],
-		logLevel: config.LOG_INFO,
-		autoWatch: false,
-		singleRun: false,
-		browsers: ['PhantomJS']
-	});
+  exclude: [],
+  frameworks: ['jasmine'],
+  logLevel: config.LOG_INFO,
+  autoWatch: false,
+  singleRun: false,
+  browsers: ['PhantomJS']
+  });
 };
 
